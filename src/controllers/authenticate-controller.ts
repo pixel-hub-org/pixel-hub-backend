befc9447bad68350d87be348cbb9da72e1eefab9
jessicaMarquess <jessica.marques.dev@gmail.com>
2025-02-25 20:46:25 -0300

    feat: auth module + authenticate jwt

---
+++ src/controllers/authenticate-controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Post } from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
+
+@Controller('/sessions')
+export class AuthenticateController {
+  constructor(private jwtService: JwtService) {}
+  @Post()
+  async handle() {
+    const token = this.jwtService.sign({ sub: 'teste-da-jesga' })
+
+    return token
+  }
+}
